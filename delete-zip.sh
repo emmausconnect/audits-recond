08409597e947134d82905e8bfce09f2a70624534
Write <541722+Write@users.noreply.github.com>
2024-11-14 22:17:54 +0100

    Update script to delete all *.zip w/o modifyin mtime of parent folder

--- delete-zip.sh
+++ delete-zip.sh
@@ -1,1 +1,8 @@
-find . -name "*.zip" -exec rm {} \; 
+find . -type f -name '*.zip' -exec sh -c '                                             root@CT135-Sites
+  for file; do
+    parent_dir=$(dirname "$file")
+    parent_mtime=$(stat -c %y "$parent_dir")
+    rm -f "$file"
+    touch -d "$parent_mtime" "$parent_dir"
+  done
+' sh {} +
